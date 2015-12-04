6abe32330aa39d3c05456fcd193c75e16b74bbe6
Kazuki Inamura <inamura@nlp.ist.i.kyoto-u.ac.jp>
2015-12-05 00:47:02 +0900

    small changes

--- CDB_Reader.py
+++ CDB_Reader.py
@@ -19,13 +19,13 @@ class CDB_Reader(object):
         basename = os.path.basename(keyMapFile)
         basename = re.sub("keymap", "", basename)
 
-        CDB0 = "%s/%s%s" % (dbdir, basename, "0")
+        CDB0 = "{}/{}{}".format(dbdir, basename, "0")
         if os.path.isfile(CDB0):
             self.mapping.append({'key': None, 'cdb': CDB0})
 
         # check for validity
         if os.path.isfile(keyMapFile) and os.path.getsize(keyMapFile) > 0:
-            CDB1 = "%s/%s%s" % (dbdir, basename, "1")
+            CDB1 = "{}/{}{}".format(dbdir, basename, "1")
             if not os.path.isfile(CDB1):
                 sys.stderr.write("The size of the keymapfile is 0, but %s \
                 exists. The size of the keymapfile should be more than 0!\n"
@@ -34,15 +34,15 @@ class CDB_Reader(object):
 
         # parse the keymap file.
         f = codecs.open(keyMapFile, 'r', 'utf-8')
+        kvptn = re.compile(r"^(.+) ([^ ]+)$")
         for line in iter(f.readline, ''):
             line = line.strip()
-            pattern = r"^(.+) ([^ ]+)$"
-            if re.match(pattern, line):
-                key, which_file = re.match(pattern, line).groups()
+            if kvptn.match(line):
+                key, which_file = kvptn.match(line).groups()
             else:
                 sys.stderr.write("malformed keymap.\n")
                 sys.exit(1)
-            CDBi = str("%s/%s" % (dbdir, which_file))
+            CDBi = str("{}/{}".format(dbdir, which_file))
             if os.path.isfile(CDBi):
                 self.mapping.append({'key': key, 'cdb': CDBi})
             else:
@@ -66,8 +66,6 @@ class CDB_Reader(object):
             nowCDB = self.mapping[0]['cdb']
             for i in range(1, len(self.mapping)):
                 nowKey = self.mapping[i]['key']
-                print nowKey
-                print nowCDB
                 if self.numerical_keys:
                     if int(searchKey) < int(nowKey):
                         break
